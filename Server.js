fc1676651a801400c25551caf5cc1251514eb556
Callum C <cpcooper34@hotmail.com>
2021-03-15 16:20:33 +1300

    feat: Added client identification clients

--- Server.js
+++ Server.js
@@ -1,3 +1,4 @@
+//https://github.com/websockets/ws
 const dotenv = require("dotenv");
 const express = require("express");
 const { NodeJSBot } = require("nodejsdiscordbot")
@@ -18,33 +19,58 @@ const DiscordHelperServer = class DiscordHelperserver extends Server {
     constructor(server, token, prefix) {
         super({ server });
 
+        //Discord bot vars
         this._token = token;
         this._prefix = prefix;
 
-        this._clients = {};
+        //Debugging - Delete later
         this.util = require("util");
 
+        //Server DB
+        //Note: ws.Server has own list of clients as this.clients
+        //All clients are assigned uniqueID on connection
+        this._discordUsers = {};
+
+        //Initialisation
         this._setupServer();
         this._setupDiscordBot();
         
+    }
+
+    _findSocket(id) {
+        return this.clients.find(ws => ws.uniqueID === id);
+    }
+
+    _handleMessage(data) {
+        
+    }
+
+    _handleCommands(ctx, args, result, command) {
+        if (command.name == "sid") {
+            //Match ctx user to a client and then ping for a sid
+            var uniqueID;
+            if (uniqueID = this._discordUsers[ctx.author.id] != undefined) {
+                //Send a request to the hunterpie plugin for the sid
+                this._findSocket(uniqueID).send();
+            } else {
+                //Send a DM for them to add their uniqueID to the DB
+                ctx.author.send()
+            }
+        }
     }
 
     _setupDiscordBot() {
         this._discordBot = new NodeJSBot(this._prefix);
         this._discordBot.once("ready", () => {
             this._discordBot.commandCollection.loadCommands();
-            console.log("Discord bot commands initialised");
         })
 
         //Add hooks
-        this._discordBot.commandCollection.on("ran", console.log);
+        this._discordBot.commandCollection.on("ran", (ctx, args, result, command) => {
+            this._handleCommands(ctx, args, result, command)
+        });
 
         this._discordBot.initialise(this._token);
-    }
-
-    _handleClose(client) {
-        //TODO(Callum): Remove connection from
-        console.log("Client disconnected");
     }
 
     _setupServer() {
@@ -52,11 +78,13 @@ const DiscordHelperServer = class DiscordHelperserver extends Server {
             const params = new URLSearchParams(request.url.substring(2));
             const uniqueID = params.get("uniqueid");
             if (uniqueID == null) {
+                //TODO(Callum): check if multiple connections from one id
                 //Decline connection
-                ws.close();
+                ws.close(4000, "No unique ID specified");
             }
-            this._clients[uniqueID] = ws;
-            this.on("close", (client) => this._handleClose(client));
+            ws.uniqueID = uniqueID;
+            //TODO(Callum): Get ws passed through to message handler
+            ws.on("message", (data) => this._handleMessage(data))
         })
     }
 }
