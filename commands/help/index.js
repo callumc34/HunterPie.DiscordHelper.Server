aaefabf8a500a70f861f057823b709d3bd4f3aa9
Callum C <cpcooper34@hotmail.com>
2021-03-16 12:25:49 +1300

    feat: Added help command

---
+++ commands/help/index.js
@@ -0,0 +1,18 @@
+const { Command } = require("nodejsdiscordbot");
+ 
+module.exports = class HelpCommand extends Command {
+                constructor () {
+                        super(
+                                "help",
+                                  [],
+                                  []);
+                }
+ 
+                async _run (ctx, args) {
+                    ctx.channel.send(`Current commands:
+                        help - see this command
+                        sid - send your session id to this channel
+                        build - post your build to this channel
+                        `)
+                }
+        }
