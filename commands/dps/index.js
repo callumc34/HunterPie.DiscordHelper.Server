4d686cd1db083edec8a9e5b35664302bb5d32fe0
Callum C <cpcooper34@hotmail.com>
2021-03-20 19:23:08 +1300

    fix: Fixed parameters for dps

--- commands/dps/index.js
+++ commands/dps/index.js
@@ -5,6 +5,7 @@ module.exports = class DPSCommand extends Command {
                         super(
                                 "dps",
                                   [],
+                                  [],
                                   ["start", "range"]);
                 }
  
